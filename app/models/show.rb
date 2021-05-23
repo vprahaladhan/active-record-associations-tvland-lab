73217da1bdae93f0c6245a0b8fe7540e14eebb40
prahal <vprahaladhan@gmail.com>
2021-05-23 10:07:27 +1000

    commit tree 5c3b283c755c7e6b12196411fa83f6ad94ea10e3

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    self.characters.map {|character| character.actor.full_name} 
+  end
 end
